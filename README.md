d04229d032398ce6187aba70c26535bf379d9fb5
MrZHLF <480970024@qq.com>
2020-03-11 11:23:50 +0800

    接口文档

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## node+express 生成 swagger 接口文档
 
+- 需要在 config 中的 db 文件,把 mongoURI 的数据库地址换成自己的
+
 ### 展示效果
 
 ![avatar](./public/12.png)
--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://root:123456@43.248.126.172:27017/admin',
+  mongoURI: 'mongodb://换成自己的',
   secretOrKey: 'secret',
   useNewUrlParser: true
 }
